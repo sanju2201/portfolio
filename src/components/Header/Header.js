90a573a2f8e766ad6c5a1311928d41a4243739e2
SanjuManna <sanjumanna2201@gmail.com>
2023-04-02 20:56:37 +0530

    Commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
 import { AiFillGithub, AiFillLinkedin, AiFillMail } from 'react-icons/ai';
-import { DiCssdeck } from 'react-icons/di';
 
 import { Container, Span, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
 
@@ -10,7 +9,7 @@ const Header = () => (
     <Div1>
       <Link href={"/"}>
         <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: "20px" }}>
-          <DiCssdeck /><Span>Portfolios</Span>
+          <Span style={{ marginLeft: "10px", fontSize: "3rem" }}>@SanjuManna</Span>
         </a>
       </Link>
     </Div1>
